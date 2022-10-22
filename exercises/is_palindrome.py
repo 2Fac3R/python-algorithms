abb9172fe1ab114f6d4c3b5edc3c470e88d8ed5e
Gustavo Navarro <genr1818@gmail.com>
2022-10-22 18:29:46 -0500

    Adding doctest

--- exercises/is_palindrome.py
+++ exercises/is_palindrome.py
@@ -1,33 +1,23 @@
-"""
-A phrase is a palindrome if, after converting all uppercase letters into lowercase letters
-and removing all non-alphanumeric characters, it reads the same forward and backward.
-Alphanumeric characters include letters and numbers.
-
-Given a string s, return true if it is a palindrome, or false otherwise.
-
-Example 1:
-Input: s = "A man, a plan, a canal: Panama"
-Output: true
-Explanation: "amanaplanacanalpanama" is a palindrome.
-
-Example 2:
-Input: s = "race a car"
-Output: false
-Explanation: "raceacar" is not a palindrome.
-
-Example 3:
-Input: s = " "
-Output: true
-Explanation: s is an empty string "" after removing non-alphanumeric characters.
-Since an empty string reads the same forward and backward, it is a palindrome.
-"""
-
-
 def is_palindrome(s: str) -> bool:
+    """
+    A phrase is a palindrome if, after converting all uppercase letters into lowercase letters
+    and removing all non-alphanumeric characters, it reads the same forward and backward.
+
+    Alphanumeric characters include letters and numbers.
+    Given a string s, return true if it is a palindrome, or false otherwise.
+
+    >>> is_palindrome("A man, a plan, a canal: Panama")
+    True
+    >>> is_palindrome("race a car")
+    False
+    >>> is_palindrome(" ")
+    True
+    """
+    # s = [i for i in s.lower() if i.isalnum()]
     s = ''.join(filter(str.isalnum, s)).lower()
     return s == s[::-1]
 
 
 if __name__ == '__main__':
-    my_string: str = "A man, a plan, a canal: Panama"
-    print(is_palindrome(my_string))
+    import doctest
+    doctest.testmod()
--- exercises/two_sum.py
+++ exercises/two_sum.py
@@ -1,41 +1,49 @@
-"""
-Two Sum
-Given an array of integers nums and an integer target, 
-return indices of the two numbers such that they add up to target.
-You may assume that each input would have exactly one solution, 
-and you may not use the same element twice.
-
-Example 1:
-
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-
-Example 2:
-
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
-
-Example 3:
-
-Input: nums = [3,3], target = 6
-Output: [0,1]
-"""
-
-
 from typing import List
 
 
 def two_sum(nums: List[int], target: int) -> List[int]:
+    """
+    Two Sum
+    Given an array of integers nums and an integer target, 
+    return indices of the two numbers such that they add up to target.
+
+    You may assume that each input would have exactly one solution, 
+    and you may not use the same element twice.
+
+    >>> two_sum([2, 7, 11, 15], 9)
+    [0, 1]
+    >>> two_sum([3, 2, 4], 6)
+    [1, 2]
+    >>> two_sum([3, 3], 6)
+    [0, 1]
+    """
+    # Brute Force - Time: O(n^2) Space: O(1)
+    # for i in range(len(nums)):
+    #     for j in range(i + 1, len(nums)):
+    #         # if nums[j] == target - nums[i]:
+    #         if nums[i] + nums[j] == target:
+    #             return [i, j]
+
+    # Hash Table - Time: O(n) Space: O(n)
+    # hashmap = {}
+    # for i in range(len(nums)):
+    #     hashmap[nums[i]] = i
+    # for i in range(len(nums)):
+    #     complement = target - nums[i]
+    #     if complement in hashmap and hashmap[complement] != i:
+    #         return [i, hashmap[complement]]
+
+    # One-pass Hash Table - Time: O(n) Space: O(n)
+    hashmap = {}
     for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
+        complement = target - nums[i]
+        if complement in hashmap:
+            return [hashmap[complement], i]
+        hashmap[nums[i]] = i
+
     return []
 
 
 if __name__ == '__main__':
-    nums: List[int] = [2, 7, 11, 15]
-    target: int = 9
-
-    print(two_sum(nums, target))
+    import doctest
+    doctest.testmod()
