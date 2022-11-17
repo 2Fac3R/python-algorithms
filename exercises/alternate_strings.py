8d47d9476d7af6b57b7f442b0c69d9e418692ce2
Gustavo Navarro <genr1818@gmail.com>
2022-11-17 16:19:05 -0600

    Typo: longer to longest variable name changed

--- exercises/alternate_strings.py
+++ exercises/alternate_strings.py
@@ -19,11 +19,11 @@ def alternate_strings(first_str: str, second_str: str) -> str:
     """
     first_str_length: int = len(first_str)
     second_str_length: int = len(second_str)
-    longer_length: int = (
+    longest_length: int = (
         first_str_length if first_str_length > second_str_length else second_str_length
     )
     output_list: list = []
-    for index in range(longer_length):
+    for index in range(longest_length):
         if index < first_str_length:
             output_list.append(first_str[index])
         if index < second_str_length:
@@ -34,4 +34,3 @@ def alternate_strings(first_str: str, second_str: str) -> str:
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
-    print(alternate_strings("AB", "XYZ"))
