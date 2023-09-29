628eede4cbfd56196fd218dccd17af2bb1c61c30
Gustavo Navarro <genr1818@gmail.com>
2023-09-29 16:08:28 -0600

    Unused params removed

--- problems/merge.py
+++ problems/merge.py
@@ -1,31 +1,29 @@
 from typing import List
 
 
-def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> List[int]:
+def merge(nums1: List[int], nums2: List[int]) -> List[int]:
     """Merge Sorted Array
 
     Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 
     Args:
         nums1 (List[int]): sorted list in non-decreasing order
-        m (int): number of elements in nums1
         nums2 (List[int]): sorted list in non-decreasing order
-        n (int): number of elements in nums2
 
     Returns:
         List[int]: a single list sorted in non-decreasing order.
 
     Tests:
-        >>> merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
+        >>> merge([1, 2, 3, 0, 0, 0], [2, 5, 6])
         [1, 2, 2, 3, 5, 6]
-        >>> merge([1], 1, [], 0)
+        >>> merge([1], [])
         [1]
-        >>> merge([0], 0, [1], 1)
+        >>> merge([0], [1])
         [1]
     """
     merge = nums1 + nums2
-    merge = sorted(list(filter(lambda x: x != 0, merge)))
-    return sorted(merge)
+    merge = sorted(list(filter(lambda x: x != 0, merge)))  # sorts and removes zeros
+    return merge
 
 
 if __name__ == '__main__':
