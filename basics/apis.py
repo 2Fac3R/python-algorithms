8ca7b027d7a09c2cf88e8dd523c5a33f1d2e8d7b
Gustavo Navarro <genr1818@gmail.com>
2023-11-15 15:17:56 -0600

    Example: Working with APIs

---
+++ basics/apis.py
@@ -0,0 +1,18 @@
+# import pip._vendor.requests as requests
+import requests, json
+
+endpoint = 'https://rickandmortyapi.com/api/character'
+
+response = requests.get(endpoint)
+data = json.loads(response.text)
+
+# print(data['info'])
+# print(data['results'])
+# print(data['results'][0]['name'])
+# print(data['results'][0]['url'])
+# print(data['results'][0]['image'])
+
+characters = data['results']
+
+for character in characters:
+    print(character['name'])
