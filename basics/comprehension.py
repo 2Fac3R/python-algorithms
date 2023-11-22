2efb44dc1a1257e1cf2ee889bb6d9ebfcdd1e70a
Gustavo Navarro <genr1818@gmail.com>
2023-11-22 17:29:15 -0600

    Generators and list comprehension examples

--- basics/comprehension.py
+++ basics/comprehension.py
@@ -12,3 +12,20 @@ print(numbers_cube)
 print(numbers_cube2)
 # print(dict(zip(numbers, numbers_cube)))
 print(odd_numbers)
+
+colors = ['black', 'white']
+sizes = ['S', 'M', 'L']
+tshirts = [
+    (color, size)
+    for color in colors
+    for size in sizes
+]
+print(tshirts)
+
+for tshirt in (f'{c} {s}' for c in colors for s in sizes):
+    # Using a generator expression would save the cost of building a list
+    print(tshirt)
+
+symbols = '$¢£¥€¤'
+ords = tuple(ord(symbol) for symbol in symbols)
+print(ords)
