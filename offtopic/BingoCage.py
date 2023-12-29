a5c33cd79e45bde9c2d894ec3880809ee3cb6e21
Gustavo Navarro <genr1818@gmail.com>
2023-12-29 13:00:16 -0600

    A BingoCage does one thing: picks items from a shuffled list.

---
+++ offtopic/BingoCage.py
@@ -0,0 +1,39 @@
+# Book: Fluent Python. Example 7-8
+import random
+
+
+class BingoCage:
+    """A BingoCage does one thing: picks items from a shuffled list.
+
+    Tests:
+        Initialize a BingoCage with a list of items:
+        >>> bingo = BingoCage(['apple', 'banana', 'cherry'])
+        >>> callable(bingo)
+        True
+
+        Pick items from the BingoCage:
+        >>> bingo.pick() in ['apple', 'banana', 'cherry']
+        True
+
+        After picking items, the BingoCage should have fewer items:
+        >>> len(bingo._items) < 3
+        True
+    """
+
+    def __init__(self, items: list) -> None:
+        self._items = list(items)
+        random.shuffle(self._items)
+
+    def pick(self):
+        try:
+            return self._items.pop()
+        except IndexError:
+            raise LookupError('pick from empty BingoCage')
+
+    def __call__(self):
+        return self.pick()
+
+
+if __name__ == '__main__':
+    from doctest import testmod
+    testmod()
